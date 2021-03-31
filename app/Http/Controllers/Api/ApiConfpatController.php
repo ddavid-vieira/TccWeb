7cd2678c739e668f3356bde37bba99d65b5b895c
David Vieira <davidvieira0110@gmail.com>
2021-03-30 22:00:30 -0300

    acaba logo tcc

--- app/Http/Controllers/Api/ApiConfpatController.php
+++ app/Http/Controllers/Api/ApiConfpatController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\ModelApi\RegisterConference;
-use App\http\Requests\CreateConferenceRequest;
+use App\Http\Requests\CreateConferenceRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ModelApi\ApiConfpatModel;
