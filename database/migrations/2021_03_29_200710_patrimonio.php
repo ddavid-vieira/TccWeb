cf5deb022f93219dbd8c26df8086aa89bcc6ae9b
David Vieira <davidvieira0110@gmail.com>
2021-03-30 21:35:01 -0300

    Alterando auth

--- database/migrations/2021_03_29_200710_patrimonio.php
+++ database/migrations/2021_03_29_200710_patrimonio.php
@@ -13,25 +13,25 @@ class Patrimonio extends Migration
      */
     public function up()
     {
-        // Schema::dropIfExists("patrimonio");
-        // Schema::create("patrimonio", function (Blueprint $table) {
-        //     $table->increments('CodPatrimonio');
-        //     $table->integer('CodSala')->unsigned();
-        //     $table->foreign('CodSala')->references('CodSala')->on('sala')->onDelete('cascade')->onUpdate('cascade');
-        //     $table->string('Unidade Gestora');
-        //     $table->string('Unidade');
-        //     $table->string('DataTombamento');
-        //     $table->string('DataGarantia');
-        //     $table->string('Denominacao');
-        //     $table->string('Marca');
-        //     $table->string('Estado');
-        //     $table->string('Finalidade');
-        //     $table->string('Depreciavel');
-        //     $table->string('Valor');
-        //     $table->boolean('Alterou');
-        //     $table->boolean('Verificado');
-        //     $table->string('NovoEstado');
-        // });
+        Schema::dropIfExists("patrimonio");
+        Schema::create("patrimonio", function (Blueprint $table) {
+            $table->increments('CodPatrimonio');
+            $table->integer('CodSala')->unsigned();
+            $table->foreign('CodSala')->references('CodSala')->on('sala')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('Unidade Gestora');
+            $table->string('Unidade');
+            $table->string('DataTombamento');
+            $table->string('DataGarantia');
+            $table->string('Denominacao');
+            $table->string('Marca');
+            $table->string('Estado');
+            $table->string('Finalidade');
+            $table->string('Depreciavel');
+            $table->string('Valor');
+            $table->boolean('Alterou');
+            $table->boolean('Verificado');
+            $table->string('NovoEstado');
+        });
     }
 
     /**
