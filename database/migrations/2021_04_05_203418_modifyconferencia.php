ac13ca44717a33a52367349ce7ad5634163d7b51
David Vieira <davidvieira0110@gmail.com>
2021-04-15 21:14:29 -0300

    dale

--- database/migrations/2021_04_05_203418_modifyconferencia.php
+++ database/migrations/2021_04_05_203418_modifyconferencia.php
@@ -14,7 +14,7 @@ class Modifyconferencia extends Migration
     public function up()
     {
         Schema::table('conferencia', function (Blueprint $table) {
-            $table->dropColumn('Data');
+            $table->timestamp('Data');
 
         });
     }
